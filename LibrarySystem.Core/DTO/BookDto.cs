51a7290c1e8ab4737bda0415be60bb46a393c6a3
Lagyha <146394918+Lagyshechka@users.noreply.github.com>
2026-04-17 21:06:13 +0300

    feat: add domain models Bool, User and BookDto

---
+++ LibrarySystem.Core/DTO/BookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibrarySystem.Core.DTO;
+
+public class BookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
---
+++ LibrarySystem.Core/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibrarySystem.Core.DTO;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    
+    public bool IsBorrowed { get; set; }
+    
+    public int? BorrowerId { get; set; }
+}
